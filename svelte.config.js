6a417b4bb64fb10ce872a5af8b067e142a57a1ed
deepfelt <dgodesign77@gmail.com>
2024-05-20 22:15:12 -0400

    migration to SvelteKit 2

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import adapter from '@sveltejs/adapter-static'
-import { vitePreprocess } from '@sveltejs/kit/vite'
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
